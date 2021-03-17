29d22bd8014f42e59d4ede2def16800c0a092cf2
Rakib Hasan <mrakibhasan199744@gmail.com>
2021-03-17 22:20:52 +0600

    Added firebase

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBHXNAehwtVaIB6qID4goSct84nQQ-bIwE",
+    authDomain: "burj-al-arab-authrecap.firebaseapp.com",
+    projectId: "burj-al-arab-authrecap",
+    storageBucket: "burj-al-arab-authrecap.appspot.com",
+    messagingSenderId: "607709000704",
+    appId: "1:607709000704:web:778c5396b8d7199dd894e5"
+  };
+  export default firebaseConfig;
